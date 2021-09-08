bef8ee58bd660e09e8075f44f669bdbcd7a15800
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-09-08 11:39:25 -0500

    - Menu preparation and design - Set the menu in each fragment related to the future actions that they will implement
    Next step: Start the layout design for each fragment. Start thinking about the Room models
    and DB.

--- app/src/main/java/com/sandoval/besttodoapp/ui/fragments/AddFragment.kt
+++ app/src/main/java/com/sandoval/besttodoapp/ui/fragments/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.sandoval.besttodoapp.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.sandoval.besttodoapp.R
 
 class AddFragment : Fragment() {
@@ -13,8 +11,15 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
