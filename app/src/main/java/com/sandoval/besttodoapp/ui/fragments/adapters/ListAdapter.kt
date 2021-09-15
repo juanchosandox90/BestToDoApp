334ab39e8d51f12651e8efad3b46334159e6cde6
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2021-09-15 15:06:38 -0500

    Databinding implementation #2
    - Start using Databinding pattern with UpdateFragment.kt
    - Move the UI color to set the priority to the binding adapter.
    - Move the navigation action from update to list into the BindingAdapter.kt
    - Bind the list update_fragment.xml to the DataBinding pattern
    - Bind the list row_layout.xml to the DataBinding pattern
    
    Note: AS has a bug with the args binding. It says that the reference is unknown and throws an error in compile time but it works fine. So leave it as it is.

--- app/src/main/java/com/sandoval/besttodoapp/ui/fragments/adapters/ListAdapter.kt
+++ app/src/main/java/com/sandoval/besttodoapp/ui/fragments/adapters/ListAdapter.kt
@@ -2,59 +2,42 @@ package com.sandoval.besttodoapp.ui.fragments.adapters
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.sandoval.besttodoapp.R
-import com.sandoval.besttodoapp.data.models.Priority
 import com.sandoval.besttodoapp.data.models.ToDoData
-import com.sandoval.besttodoapp.ui.fragments.ListFragmentDirections
-import kotlinx.android.synthetic.main.row_layout.view.*
+import com.sandoval.besttodoapp.databinding.RowLayoutBinding
 
 open class ListAdapter : RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
     private var dataList = emptyList<ToDoData>()
 
+    class MyViewHolder(private val binding: RowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(toDoData: ToDoData) {
+            binding.toDoData = toDoData
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
     override fun getItemCount(): Int {
         return dataList.size
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_layout, parent, false)
-        return MyViewHolder(view)
+        return MyViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.itemView.titleText.text = dataList[position].title
-        holder.itemView.descriptionText.text = dataList[position].description
-
-        holder.itemView.rowBackground.setOnClickListener {
-            val action =
-                ListFragmentDirections.actionListFragmentToUpdateFragment(dataList[position])
-            holder.itemView.findNavController().navigate(action)
-        }
-
-        when (dataList[position].priority) {
-            Priority.HIGH -> holder.itemView.priorityIndicator.setCardBackgroundColor(
-                ContextCompat.getColor(
-                    holder.itemView.context,
-                    R.color.redPriority
-                )
-            )
-            Priority.MEDIUM -> holder.itemView.priorityIndicator.setCardBackgroundColor(
-                ContextCompat.getColor(holder.itemView.context, R.color.yellowPriority)
-            )
-            Priority.LOW -> holder.itemView.priorityIndicator.setCardBackgroundColor(
-                ContextCompat.getColor(
-                    holder.itemView.context,
-                    R.color.greenPriority
-                )
-            )
-        }
+        val currentItem = dataList[position]
+        holder.bind(currentItem)
     }
 
     @SuppressLint("NotifyDataSetChanged")
